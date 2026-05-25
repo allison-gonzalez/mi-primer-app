662d4a4705fa252a55af9f6652fb9d69a7f82f68
Allison Gonzalez <allison.gonzalez.23s@utzmg.edu.mx>
2026-05-25 11:49:53 -0600

    reproduce audio

--- reloj/src/main/java/com/example/textoapp/presentation/Prueba.kt
+++ reloj/src/main/java/com/example/textoapp/presentation/Prueba.kt
@@ -3,11 +3,25 @@ package com.example.textoapp.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import com.example.textoapp.R
+import android.media.MediaPlayer
+import android.widget.Button
 
 class Prueba : ComponentActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.prueba)
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.wiwiwi)
+
+        val boton: Button = findViewById(R.id.boton);
+
+        boton.setOnClickListener {
+            if (!mediaPlayer.isPlaying) {
+                mediaPlayer.start()
+            }
+        }
     }
 }
